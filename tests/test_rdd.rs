dda9dd0321f4df34e9fa1768957c4e1371506287
rajasekarv <rajasekar3eg@gmail.com>
2020-01-20 08:14:18 +0400

    merged test_union with test_rdd

--- tests/test_rdd.rs
+++ tests/test_rdd.rs
@@ -4,6 +4,9 @@ use native_spark::*;
 use std::fs::{create_dir_all, remove_dir_all, File};
 use std::io::prelude::*;
 use std::sync::Arc;
+use native_spark::rdd::CoGroupedRdd;
+use native_spark::partitioner::HashPartitioner;
+use serde_traitobject::Arc as SerArc;
 
 #[macro_use]
 extern crate serde_closure;
@@ -378,4 +381,30 @@ fn test_coalesced() -> Result<()> {
     }
 
     Ok(())
+}
+
+#[test]
+fn test_union_with_unique_partitioner() {
+    let sc = CONTEXT.clone();
+    let partitioner = HashPartitioner::<i32>::new(2);
+    let co_grouped = || {
+        let rdd = vec![
+            (1i32, "A".to_string()),
+            (2, "B".to_string()),
+            (3, "C".to_string()),
+            (4, "D".to_string()),
+        ];
+        let rdd0 = SerArc::new(sc.parallelize(rdd.clone(), 2))
+            as SerArc<dyn Rdd<Item = (i32, String)>>;
+        let rdd1 =
+            SerArc::new(sc.parallelize(rdd, 2)) as SerArc<dyn Rdd<Item = (i32, String)>>;
+        CoGroupedRdd::<i32>::new(
+            vec![rdd0.get_rdd_base().into(), rdd1.get_rdd_base().into()],
+            Box::new(partitioner.clone()),
+        )
+    };
+    let rdd0 = co_grouped();
+    let rdd1 = co_grouped();
+    let res = rdd0.union(rdd1.get_rdd()).unwrap().collect().unwrap();
+    assert_eq!(res.len(), 8);
 }
