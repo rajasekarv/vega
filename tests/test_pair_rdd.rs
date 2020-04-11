ff88906446cc112e0ef0118da2f64f2266f68017
Ignacio Duart <iduartgomez@gmail.com>
2020-04-11 21:33:22 +0200

    Count by value moved to pair_rdd

--- tests/test_pair_rdd.rs
+++ tests/test_pair_rdd.rs
@@ -83,3 +83,17 @@ fn test_join() {
     .collect::<Vec<_>>();
     assert_eq!(expected, res);
 }
+
+#[test]
+fn test_count_by_value() -> Result<()> {
+    let sc = CONTEXT.clone();
+
+    let rdd = sc.parallelize(vec![1i32, 2, 1, 2, 2], 2);
+    let rdd = rdd.count_by_value();
+    let res = rdd.collect().unwrap();
+
+    assert_eq!(res.len(), 2);
+    itertools::assert_equal(res, vec![(1, 2), (2, 3)]);
+
+    Ok(())
+}
